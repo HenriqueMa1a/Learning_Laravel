d820ad6d4e3be86738cba5d957e191606a7c216c
HenriqueMa1a <henriquemaya01@icloud.com>
2023-04-28 00:29:33 -0300

    listar registros do Banco com o Laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('users.index');
+        $users = User::get(); // ou ::all
+        return view('users.index', compact('users'));
     }
     public function show($id)
     {
-        dd('users.show', $id);
+
+        // $user = User::where('id', '=', $id)->first(); //poderia passar sem o valor de comparação
+        if(!$user = User::find($id))
+            return redirect()-> route('users.index');              //back();  volta de onde veio
+
+        return view('users.show', compact('user'));
     }
 }
